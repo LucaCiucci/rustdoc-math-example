7739deb9b2969db93b5405b9870c39c247d55ad8
Luca Ciucci <luca.ciucci99@gmail.com>
2023-03-14 09:58:41 +0100

    minimum working example

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rustdoc-math-example
  Example for using latex equations in rustdoc
+
+## Generating documentation
+
+```bash
+cargo rustdoc --open
+```
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+
+/// This is a doc comment
+/// 
+/// This is an inline equation: $x^2 + y^2 = z^2$
+/// 
+/// This is a block equation:
+/// $$
+/// x^2 + y^2 = z^2
+/// $$
+fn main() {
+    println!("Hello, world!");
+}
